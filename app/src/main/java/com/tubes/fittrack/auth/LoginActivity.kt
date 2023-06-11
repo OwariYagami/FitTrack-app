15fb7db39fd5d15703f1de2b51bfe1b48d57c35b
Owari Yagami <hendisholeh1@gmail.com>
2023-06-11 13:27:33 +0700

    first commit

---
+++ app/src/main/java/com/tubes/fittrack/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tubes.fittrack.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tubes.fittrack.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/tubes/fittrack/auth/SplachScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.tubes.fittrack.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tubes.fittrack.R
+
+class SplachScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splach_screen)
+    }
+}
