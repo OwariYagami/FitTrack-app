3fc88a27cd8a1eff276695e8029ef18ade6da64b
fauqaji <133103837+fauqaji@users.noreply.github.com>
2023-06-11 17:55:00 +0700

    commit
    Intent Splach -> Login -> Register

--- app/src/main/java/com/tubes/fittrack/auth/LoginActivity.kt
+++ app/src/main/java/com/tubes/fittrack/auth/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.tubes.fittrack.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.tubes.fittrack.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnIntent : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnIntent = findViewById(R.id.btn_register)
+
+        btnIntent.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_register ->{
+                val intentB = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intentB)
+            }
+        }
     }
 }
--- app/src/main/java/com/tubes/fittrack/auth/SplachScreenActivity.kt
+++ app/src/main/java/com/tubes/fittrack/auth/SplachScreenActivity.kt
@@ -1,12 +1,32 @@
 package com.tubes.fittrack.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.tubes.fittrack.R
 
-class SplachScreenActivity : AppCompatActivity() {
+class SplachScreenActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnIntent : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splach_screen)
+
+        btnIntent = findViewById(R.id.btn_mulai)
+
+        btnIntent.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_mulai ->{
+                val intentB = Intent(this@SplachScreenActivity, LoginActivity::class.java)
+                startActivity(intentB)
+            }
+        }
+    }
+
+
 }
