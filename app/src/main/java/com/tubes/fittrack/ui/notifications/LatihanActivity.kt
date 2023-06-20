d563a3d37e08d8b1df5951bf999ea6b3c3767a81
Owari Yagami <hendisholeh1@gmail.com>
2023-06-20 19:14:25 +0700

    Membuat Layout aktivitas

---
+++ app/src/main/java/com/tubes/fittrack/ui/notifications/LatihanActivity.kt
@@ -0,0 +1,12 @@
+package com.tubes.fittrack.ui.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tubes.fittrack.R
+
+class LatihanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latihan)
+    }
+}
