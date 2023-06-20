a3d49821b684271aed711696429b86a8b366e6f9
fauqaji <133103837+fauqaji@users.noreply.github.com>
2023-06-20 13:19:43 +0700

    layout edit profile

---
+++ app/src/main/java/com/tubes/fittrack/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.tubes.fittrack.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tubes.fittrack.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
--- app/src/main/java/com/tubes/fittrack/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/tubes/fittrack/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.tubes.fittrack.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tubes.fittrack.R
+import com.tubes.fittrack.auth.RegisterActivity
+import com.tubes.fittrack.databinding.ActivityLoginBinding
+import com.tubes.fittrack.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,7 +25,7 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,10 +37,20 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        binding.btnEdit.setOnClickListener {
+            val intent = Intent(activity, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+        return root;
     }
+
 
     companion object {
         /**
@@ -57,4 +71,6 @@ class ProfileFragment : Fragment() {
                 }
             }
     }
+
+
 }
